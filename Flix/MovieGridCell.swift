225ab36f4dc941eb78967346bcce109989e4408d
Eyoab Asrat <eyoabasrat@gmail.com>
2021-02-26 17:47:40 -0500

    flix part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Eyoab Asrat on 2/26/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
